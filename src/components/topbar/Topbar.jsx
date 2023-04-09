b0a1ce2a7383123f6e7a746c673a78e646014136
olgarpsb <77417318+olgarpsb@users.noreply.github.com>
2023-04-09 01:15:19 -0500

    initial

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,25 @@
+import "./topbar.scss"
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left" >
+        <a href="#intro" className="logo">genius.</a>
+        <div className="itemContainer">
+            {/* <Person className="icon" />
+            <span>+44 924 12 74</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>safak@genius.com</span> */}
+          </div>
+        </div>
+        <h1>Home</h1>
+        <div className="right">
+           Right
+        </div>
+      </div>
+    </div>
+  )
+}
